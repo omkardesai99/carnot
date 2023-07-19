5775aa606a09bba01e0dec199b874bd04c6881dc
Omkar Desai <omkarsdesai100@gmail.com>
2023-07-19 12:00:27 +0530

    minor changes in cors origin in settings and requirements.txt

--- carnot_project/carnot_project/settings.py
+++ carnot_project/carnot_project/settings.py
@@ -59,10 +59,10 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
 ]
 
 ROOT_URLCONF = "carnot_project.urls"
