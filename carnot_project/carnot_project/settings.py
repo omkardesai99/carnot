ad23d2fa4ce6b19d814097cd924b9d6a48014c2a
Omkar Desai <omkarsdesai100@gmail.com>
2023-07-19 10:47:45 +0530

    unhashed csrf in settings

--- carnot_project/carnot_project/settings.py
+++ carnot_project/carnot_project/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CSRF_TRUSTED_ORIGINS = [
-    "*",
     "https://django-server-production-ef28.up.railway.app",
 ]
 # Application definition
@@ -50,7 +49,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # "django.middleware.csrf.CsrfViewMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
